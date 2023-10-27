3731f58126c26da4233e9390edbcc2486d05b9ac
Przemek Paziewski <ppaazz56@gmail.com>
2023-10-27 20:41:02 +0200

    refactor, remove eslint no spread props rule

--- src/components/app-input.tsx
+++ src/components/app-input.tsx
@@ -2,7 +2,6 @@
 
 import { TextField, TextFieldProps } from '@mui/material';
 
-// eslint-disable-next-line react/jsx-props-no-spreading
 const AppInput = (props: TextFieldProps) => <TextField {...props} />;
 
 export default AppInput;
