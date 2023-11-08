26edd4c8d2625ae39992350779bb96a6dd0faef9
Przemysław Paziewski <86775840+przemyslaw-paziewski@users.noreply.github.com>
2023-11-08 21:40:46 +0100

    Feat filters submission add (#11)
    * feat: trainerAccount profile link mobile
    
    * feat: add filters submissions

--- src/app/(content)/submissions/layout.tsx
+++ src/app/(content)/submissions/layout.tsx
@@ -2,14 +2,22 @@ import { ReactNode } from 'react';
 import MobileNavbarLink from '@/app/(content)/_components/navbar/mobile-navbar/mobile-navbar-link';
 import AppLogo from '@/components/app-logo';
 
-const SubmissionLayout = ({ children }: { children: ReactNode }) => (
-  <section>
-    <div className="align-center my-5 flex w-full justify-between lg:invisible">
-      <AppLogo />
-      <MobileNavbarLink aria-label="Ustawienia" href="/settings" icon="settings" />
-    </div>
-    {children}
-  </section>
-);
+const SubmissionLayout = ({ children }: { children: ReactNode }) => {
+  // TODO CHECK ACCOUNT TYPE
+  const isTrainerAccount = true;
+
+  return (
+    <section className="my-5 flex w-full flex-col gap-10">
+      <div className="align-center flex w-full justify-between lg:invisible">
+        <AppLogo />
+        <div className="flex gap-5">
+          <MobileNavbarLink aria-label="Ustawienia" href="/settings" icon="settings" />
+          {isTrainerAccount && <MobileNavbarLink aria-label="Profil" href="/profile" icon="profile" />}
+        </div>
+      </div>
+      {children}
+    </section>
+  );
+};
 
 export default SubmissionLayout;
--- src/app/(content)/submissions/page.tsx
+++ src/app/(content)/submissions/page.tsx
@@ -1,14 +1,18 @@
+import FilterTabs from './_components/filter-tabs';
 import OrdersCard from './_components/orders-card';
 
-const OrdersPage = () => (
-  <div>
-    <h1>Orders</h1>
-    <div className="grid grid-cols-3 gap-4">
-      {[1, 2].map((key) => (
-        <OrdersCard key={key} />
-      ))}
+const SubmissionsPage = ({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) => (
+  <>
+    <h1 className="text-2xl text-white">Twoje zgłoszenia</h1>
+    <div className="flex flex-col gap-5">
+      <FilterTabs defaultFilter={searchParams?.filter} />
+      <div className="grid grid-cols-3 gap-4">
+        {[1, 2].map((key) => (
+          <OrdersCard key={key} />
+        ))}
+      </div>
     </div>
-  </div>
+  </>
 );
 
-export default OrdersPage;
+export default SubmissionsPage;
