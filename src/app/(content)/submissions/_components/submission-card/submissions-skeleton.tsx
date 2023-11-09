edb7c659a24b1a924442162b1702258089347995
Przemek Paziewski <ppaazz56@gmail.com>
2023-11-09 21:17:10 +0100

    feat/animate puse skeleton add

--- src/app/(content)/submissions/_components/submission-card/submissions-skeleton.tsx
+++ src/app/(content)/submissions/_components/submission-card/submissions-skeleton.tsx
@@ -2,11 +2,11 @@ import { SubmissionCardContainer } from './submission-card';
 
 const SubmissionCardSkeleton = () => (
   <SubmissionCardContainer>
-    <div className="relative h-60 w-full rounded-xl bg-[#0D1116] min-[450px]:h-40 lg:h-60" />
-    <div className="w-full rounded-full bg-[#0D1116]">
-      <h5 className="invisible select-none text-sm font-bold text-white lg:text-xl">Imię </h5>
+    <div className="relative h-60 w-full animate-pulse rounded-xl bg-[#0D1116] min-[450px]:h-40 lg:h-60" />
+    <div className="w-full animate-pulse rounded-full bg-[#0D1116]">
+      <h5 className="invisible animate-pulse select-none text-sm font-bold text-white lg:text-xl">Imię </h5>
     </div>
-    <div className="w-full select-none rounded-full bg-yellow-400 py-[10px] text-center text-xs font-bold text-yellow-400 lg:text-base">
+    <div className="w-full animate-pulse select-none rounded-full bg-yellow-400 py-[10px] text-center text-xs font-bold text-yellow-400 lg:text-base">
       Szczegóły
     </div>
   </SubmissionCardContainer>
