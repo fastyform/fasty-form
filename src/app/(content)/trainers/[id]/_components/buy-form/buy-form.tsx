c729d0184ca4f557a6644c1b30294f3ec7879958
Przemysław Paziewski <86775840+przemyslaw-paziewski@users.noreply.github.com>
2023-12-09 15:32:50 +0100

    fix, buttons states changes (#44)

--- src/app/(content)/trainers/[id]/_components/buy-form/buy-form.tsx
+++ src/app/(content)/trainers/[id]/_components/buy-form/buy-form.tsx
@@ -33,6 +33,7 @@ const SubmitButton = ({ disabled, ...props }: ButtonHTMLAttributes<HTMLButtonEle
       className={twMerge(
         'flex w-full max-w-sm items-center justify-center rounded-full bg-yellow-400 px-[30px] py-[18px] text-base font-bold normal-case tracking-normal text-[#0D1116] transition-opacity hover:opacity-80',
         (pending || disabled) && 'pointer-events-none bg-gray-600',
+        disabled && '!text-white/60',
       )}
     >
       <ButtonChildren disabled={!!disabled} pending={pending} />
