ed9bd28d1481e33ae69d693e0631941b61d245c1
Kacper Zabielski <beelzick.dev@gmail.com>
2023-12-29 20:42:46 +0100

    Fix submission card paid state bg

--- src/app/(content)/submissions/_components/submission-card/submission-card-image-fallback.tsx
+++ src/app/(content)/submissions/_components/submission-card/submission-card-image-fallback.tsx
@@ -1,12 +1,7 @@
 import Image from 'next/image';
 
 const SubmissionCardImageFallback = () => (
-  <Image
-    fill
-    alt="Brak miniaturki zgłoszenia"
-    className="rounded-xl bg-[#0D1116] object-contain"
-    src="/image-placeholder.png"
-  />
+  <Image fill alt="Brak miniaturki zgłoszenia" className="object-contain" src="/image-placeholder.png" />
 );
 
 export default SubmissionCardImageFallback;
