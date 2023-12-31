69b6f5fcb2d3cbd4f92b8e657779cee7f9ab4c6c
Przemek Paziewski <ppaazz56@gmail.com>
2023-12-31 16:57:16 +0100

    refactor/change profile slug input

--- src/app/(content)/_components/onboarding-form/_utils/index.ts
+++ src/app/(content)/_components/onboarding-form/_utils/index.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
+const ALLOWED_SLUG_CHARS_REGEX = /^[a-z0-9\-_]+$/g;
+
 export type OnboardingFormValues = z.infer<typeof onboardingFormSchema>;
 
 export const onboardingFormSchema = z.object({
@@ -10,6 +12,7 @@ export const onboardingFormSchema = z.object({
     .max(50, 'Nazwa profilu może zawierać maksymalnie 50 znaków.'),
   profileSlug: z
     .string({ required_error: 'Wprowadź link do profilu.' })
+    .regex(ALLOWED_SLUG_CHARS_REGEX, 'Link może zawierać wyłącznie małe litery, cyfry, łączniki (-) i podkreślenia (_)')
     .min(1, 'Wprowadź link do profilu.')
     .max(50, 'Link do profilu może zawierać maksymalnie 50 znaków.'),
 });
--- src/app/(content)/_components/onboarding-form/onboarding-form.tsx
+++ src/app/(content)/_components/onboarding-form/onboarding-form.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useFormState } from 'react-dom';
 import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import ProfileSlugInput from '@/app/(content)/_components/onboarding-form/_components/profile-slug-input';
+import { InputAdornment } from '@mui/material';
+import slugify from 'slugify';
 import AppButtonSubmit from '@/components/app-button-submit';
 import AppFormState from '@/components/app-form-error';
 import AppInputForm from '@/components/app-input/app-input-form';
@@ -13,14 +14,22 @@ import { formDefaultState } from '@/utils/form';
 import actionOnBoarding from './_actions/action-onboarding';
 import { onboardingFormSchema, OnboardingFormValues } from './_utils';
 
+export const NOT_ALLOWED_SLUG_CHARS_REGEX = /[*+~.()'"!:@#^`=[{\]}\\]/g;
+const slugifyWithOptions = (text: string) =>
+  slugify(text, { replacement: '-', lower: true, remove: NOT_ALLOWED_SLUG_CHARS_REGEX });
+
 const OnboardingForm = () => {
-  const [isLoading, setIsLoading] = useState(false);
   const [state, formAction] = useFormState(actionOnBoarding, formDefaultState);
-  const { control, handleSubmit, formState, watch, getFieldState, setValue, setError } = useForm<OnboardingFormValues>({
-    resolver: zodResolver(onboardingFormSchema),
-    defaultValues: { servicePrice: 1, profileName: '' },
-    mode: 'onTouched',
-  });
+  const { control, handleSubmit, formState, watch, getFieldState, setValue, setError, resetField } =
+    useForm<OnboardingFormValues>({
+      resolver: zodResolver(onboardingFormSchema),
+      defaultValues: { servicePrice: 1, profileName: '', profileSlug: '' },
+      mode: 'onTouched',
+    });
+
+  const profileSlugInputState = getFieldState('profileSlug');
+  const profileSlugInputValue = watch('profileSlug');
+  const profileNameInputValue = watch('profileName');
 
   const handleFormAction = (data: FormData) => handleSubmit(async () => formAction(data))();
 
@@ -28,7 +37,7 @@ const OnboardingForm = () => {
     if (state.message === 'Twój link do profilu nie jest unikalny. Spróbuj inny.') {
       setError('profileSlug', {}, { shouldFocus: true });
     }
-  }, [setError, state.message, state]);
+  }, [setError, state.message]);
 
   return (
     <form action={handleFormAction} className="flex w-full flex-col gap-5">
@@ -48,19 +57,46 @@ const OnboardingForm = () => {
         </div>
         <div className="flex flex-col gap-2.5 ">
           <span className="text-white">Nazwa profilu</span>
-          <AppInputForm<OnboardingFormValues> control={control} fieldName="profileName" />
+          <AppInputForm<OnboardingFormValues>
+            control={control}
+            fieldName="profileName"
+            onBlur={(e) => {
+              if (!profileNameInputValue) return;
+
+              if (!profileSlugInputState.isTouched || !profileSlugInputValue) {
+                setValue('profileSlug', slugifyWithOptions(e.target.value), {
+                  shouldValidate: true,
+                  shouldDirty: true,
+                });
+              }
+            }}
+          />
         </div>
-        <ProfileSlugInput
-          control={control}
-          getFieldState={getFieldState}
-          setIsLoading={setIsLoading}
-          setValue={setValue}
-          watch={watch}
-        />
+        <div className="flex flex-col gap-2.5 ">
+          <span className="text-white">Link do profilu</span>
+          <span className="text-xs text-white">
+            Pamiętaj, że link do Twojego profilu jest stały i nie podlega zmianie. Możesz go swobodnie udostępniać swoim
+            klientom, dlatego wybierz go mądrze!
+          </span>
+          <AppInputForm<OnboardingFormValues>
+            control={control}
+            fieldName="profileSlug"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <span className="text-xs text-white/50">fastyform.com/trainers/</span>
+                </InputAdornment>
+              ),
+            }}
+            onBlur={(e) => {
+              if (!e.target.value) {
+                resetField('profileSlug', { keepError: true });
+              }
+            }}
+          />
+        </div>
       </div>
-      <AppButtonSubmit disabled={isLoading} isValid={formState.isValid}>
-        Przejdź dalej
-      </AppButtonSubmit>
+      <AppButtonSubmit isValid={formState.isValid}>Przejdź dalej</AppButtonSubmit>
     </form>
   );
 };
