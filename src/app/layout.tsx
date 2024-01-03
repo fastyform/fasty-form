7d1eb8a14d109467262361ca15cafceb528d321e
Przemysław Paziewski <86775840+przemyslaw-paziewski@users.noreply.github.com>
2024-01-03 19:20:09 +0100

    refactor/get cookies consent action add (#82)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { ToastContainer } from 'react-toastify';
 import type { Metadata } from 'next';
 import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
-import { cookies } from 'next/headers';
 import Constants from '@/utils/constants';
 import Analytics from './_components/analytics';
 import CookiesModal from './_components/cookies-modal';
@@ -14,19 +13,15 @@ export const metadata: Metadata = {
   description: `${Constants.APP_NAME} - szybko sprawdź swoją technikę przy pomocy trenera`,
 };
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  const cookiesConsent = cookies().get('cookiesConsent');
-
-  return (
-    <html lang="pl">
-      <body className={plusJakartaSans.className} id="body">
-        <ToastContainer toastClassName="bg-[#0D1116] border-gray-600 border rounded-lg" />
-        <CookiesModal cookiesConsent={cookiesConsent} />
-        <Analytics />
-        <div className="min-h-screen-responsive w-full bg-[#0D1116]">{children}</div>
-      </body>
-    </html>
-  );
-};
+const RootLayout = async ({ children }: { children: React.ReactNode }) => (
+  <html lang="pl">
+    <body className={plusJakartaSans.className} id="body">
+      <ToastContainer toastClassName="bg-[#0D1116] border-gray-600 border rounded-lg" />
+      <CookiesModal />
+      <Analytics />
+      <div className="min-h-screen-responsive w-full bg-[#0D1116]">{children}</div>
+    </body>
+  </html>
+);
 
 export default RootLayout;
