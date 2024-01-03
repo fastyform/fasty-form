ad304e5739971dee92c6932618f384c1fef64167
Kacper Zabielski <beelzick.dev@gmail.com>
2024-01-03 21:04:34 +0100

    Fix footer alignment

--- src/app/(auth)/_components/auth-footer.tsx
+++ src/app/(auth)/_components/auth-footer.tsx
@@ -5,7 +5,7 @@ const AuthFooter = ({ shouldNavigateBack }: { shouldNavigateBack?: boolean }) =>
   const getHrefFormatted = (href: Route) => (shouldNavigateBack ? `${href}?should-navigate-back=true` : href) as Route;
 
   return (
-    <div className="flex justify-center gap-5 pt-10 text-sm text-white">
+    <div className="flex flex-wrap justify-center gap-5 pt-10 text-sm text-white">
       <Link href={getHrefFormatted('/terms-of-service')}>Regulamin</Link>
       <Link href={getHrefFormatted('/privacy-policy')}>Polityka prywatności</Link>
       <Link href={getHrefFormatted('/contact')}>Kontakt</Link>{' '}
