983a7399f37ecd620f2411c299b35a1dc752cdf7
Przemek Paziewski <ppaazz56@gmail.com>
2024-01-04 22:30:28 +0100

    fix/migration fix

--- supabase/migrations/20240104140151_stripe_onboardingz_changes.sql
+++ supabase/migrations/20240104140151_stripe_onboardingz_changes.sql
@@ -1,7 +1,11 @@
-create type "public"."stripe_onboarding_status" as enum ('verified', 'unverified', 'pending_verification');
+CREATE TYPE "public"."stripe_onboarding_status_enum" AS ENUM (
+    'verified',
+    'unverified',
+    'pending_verification'
+);
 
 alter table "public"."trainers_details" drop column "is_onboarded_stripe";
 
-alter table "public"."trainers_details" add column "stripe_onboarding_status" stripe_onboarding_status not null default 'unverified'::stripe_onboarding_status;
+alter table "public"."trainers_details" add column "stripe_onboarding_status" stripe_onboarding_status_enum not null default 'unverified'::stripe_onboarding_status_enum;
 
 
