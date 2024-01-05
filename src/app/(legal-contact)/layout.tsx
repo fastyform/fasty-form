823ed9521939c43446d29a3b25d9c2900e03d5f0
Przemek Paziewski <ppaazz56@gmail.com>
2024-01-05 21:17:08 +0100

    fix/useSearchParams client side error

--- src/app/(legal-contact)/layout.tsx
+++ src/app/(legal-contact)/layout.tsx
@@ -1,10 +1,18 @@
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 import BackLink from './_components/back-link';
 
 const LegalSupportLayout = ({ children }: { children: ReactNode }) => (
   <div className="min-h-screen-responsive flex flex-col">
     <div className="ml-auto mr-auto flex w-full max-w-screen-2xl flex-col gap-5 px-5 py-10 text-white md:gap-10">
-      <BackLink />
+      <Suspense
+        fallback={
+          <div className="w-fit animate-pulse select-none rounded-full bg-[#1E2226] px-5 text-xl">
+            <span className="invisible">Powrót</span>
+          </div>
+        }
+      >
+        <BackLink />
+      </Suspense>
       {children}
     </div>
   </div>
