a5850143a14b70d45b3882be68097b596ea12533
Kacper Zabielski <beelzick.dev@gmail.com>
2024-01-06 17:55:08 +0100

    Fix broken expired submission video

--- src/app/(content)/submissions/[id]/(submission)/_components/submission-video.tsx
+++ src/app/(content)/submissions/[id]/(submission)/_components/submission-video.tsx
@@ -1,44 +1,38 @@
-import { HeadObjectCommand } from '@aws-sdk/client-s3';
+import { GetObjectCommand, HeadObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import getSubmissionById from '@/app/(content)/submissions/[id]/(submission)/_utils/get-submission-by-id';
 import { removeFileExtension } from '@/utils';
-import Constants from '@/utils/constants';
-import s3Client, { BUCKET_NAME_PROCESSED } from '@/utils/s3';
+import s3Client, { BUCKET_NAME_PROCESSED, BUCKET_NAME_UNPROCESSED } from '@/utils/s3';
 
 const UNPROCESSED_VIDEO_URL_EXPIRATION_TIME_IN_SECONDS = 60 * 10; // 10 minutes
-const PROCESSED_VIDEO_URL_EXPIRATION_TIME_IN_SECONDS = 60 * 60; // 1 hour
-
-export const getVideoUrlExpirationTimeInSeconds = (isProcessed: boolean) => {
-  if (isProcessed) return PROCESSED_VIDEO_URL_EXPIRATION_TIME_IN_SECONDS;
-
-  return UNPROCESSED_VIDEO_URL_EXPIRATION_TIME_IN_SECONDS;
-};
-
-export const getProcessedVideoKey = (videoKey: string) => `${removeFileExtension(videoKey)}.mp4`;
+const PROCESSED_VIDEO_URL_EXPIRATION_TIME_IN_SECONDS = 60 * 60 * 2; // 2 hours
 
 const SubmissionVideo = async ({ submissionId }: { submissionId: string }) => {
   const submission = await getSubmissionById(submissionId);
 
   if (!submission.video_key) throw new Error('No video key');
+  const videoKey = submission.video_key;
+  const processedVideoKey = `${removeFileExtension(videoKey)}.mp4`;
 
   let isVideoProcessed = false;
 
   try {
-    await s3Client.send(
-      new HeadObjectCommand({ Bucket: BUCKET_NAME_PROCESSED, Key: getProcessedVideoKey(submission.video_key) }),
-    );
+    await s3Client.send(new HeadObjectCommand({ Bucket: BUCKET_NAME_PROCESSED, Key: processedVideoKey }));
     isVideoProcessed = true;
   } catch {}
 
-  const response = await fetch(
-    `${Constants.ORIGIN_URL}/api/video/url/${submission.video_key}?isProcessed=${isVideoProcessed}`,
+  const videoUrl = await getSignedUrl(
+    s3Client,
+    new GetObjectCommand({
+      Bucket: isVideoProcessed ? BUCKET_NAME_PROCESSED : BUCKET_NAME_UNPROCESSED,
+      Key: isVideoProcessed ? processedVideoKey : videoKey,
+    }),
     {
-      method: 'GET',
-      next: { revalidate: getVideoUrlExpirationTimeInSeconds(isVideoProcessed) },
+      expiresIn: isVideoProcessed
+        ? PROCESSED_VIDEO_URL_EXPIRATION_TIME_IN_SECONDS
+        : UNPROCESSED_VIDEO_URL_EXPIRATION_TIME_IN_SECONDS,
     },
   );
-  if (!response.ok) throw new Error('Failed to get video url');
-
-  const { videoUrl } = (await response.json()) as { videoUrl: string };
 
   return (
     <video
