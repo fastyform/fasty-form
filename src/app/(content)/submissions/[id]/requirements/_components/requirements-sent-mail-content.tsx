24e425b9a3640a79b27444033aa0dc1936f6d5df
Przemek Paziewski <ppaazz56@gmail.com>
2024-02-20 21:21:02 +0100

    fix(mail): requirement sent mail contnent space add

--- src/app/(content)/submissions/[id]/requirements/_components/requirements-sent-mail-content.tsx
+++ src/app/(content)/submissions/[id]/requirements/_components/requirements-sent-mail-content.tsx
@@ -17,7 +17,7 @@ const RequirementsSentMailContent = ({ trainerName, submissionId }: { trainerNam
     <br /> Jesteśmy przekonani, że Twoja wiedza i doświadczenie będą kluczowe w osiąganiu celów Twoich podopiecznych.
     <br />
     <br /> Jeśli masz jakiekolwiek pytania lub potrzebujesz wsparcia, skontaktuj się z nami przez
-    <a href={`${Constants.ORIGIN_URL}/contact`}>formularz kontaktowy</a>. Jesteśmy tutaj, aby Ci pomóc.
+    <a href={`${Constants.ORIGIN_URL}/contact`}>formularz kontaktowy </a>. Jesteśmy tutaj, aby Ci pomóc.
     <br />
     <br /> Dziękujemy za Twoje zaangażowanie i ciężką pracę w {Constants.APP_NAME}.
     <br />
