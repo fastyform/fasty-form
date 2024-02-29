ff0c07cce708fe52428ee55f73fa2604ae9442d7
Kacper Zabielski <86677657+beelzick@users.noreply.github.com>
2024-02-29 17:37:39 +0100

    fix: payment webhook (#151)

--- src/app/(content)/api/stripe/payment/webhook/route.tsx
+++ src/app/(content)/api/stripe/payment/webhook/route.tsx
@@ -1,6 +1,7 @@
 import { render } from '@react-email/render';
 import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
+import Stripe from 'stripe';
 import getStripe from '@/app/(stripe)/stripe/_utils/get-stripe';
 import MailTemplate from '@/utils/mail/mail-template';
 import { sendMail } from '@/utils/mail/send-mail';
@@ -39,6 +40,16 @@ export async function POST(req: Request) {
         throw new Error('Metadata is empty');
       }
 
+      const paymentIntent = await stripe.paymentIntents.retrieve(
+        session.payment_intent as string,
+        {
+          expand: ['latest_charge.balance_transaction'],
+        },
+        {
+          stripeAccount: event.account,
+        },
+      );
+
       const { data: submission, error } = await supabase
         .from('submissions')
         .insert({
@@ -47,6 +58,9 @@ export async function POST(req: Request) {
           trainer_id: session.metadata.trainerId,
           status: 'paid',
           id: session.metadata.submissionId,
+          price_in_grosz: (
+            (paymentIntent.latest_charge as Stripe.Charge).balance_transaction as Stripe.BalanceTransaction
+          ).net,
         })
         .select('id, client_id, trainers_details (profile_name, profile_slug)')
         .single();
@@ -74,37 +88,6 @@ export async function POST(req: Request) {
           </MailTemplate>,
         ),
       });
-    } catch (error) {
-      return NextResponse.json(
-        {
-          message: `Creating submission error: ${error}`,
-          ok: false,
-        },
-        { status: 400 },
-      );
-    }
-  }
-
-  if (event.type === 'charge.succeeded') {
-    const charge = event.data.object;
-
-    try {
-      const balance = await stripe.balanceTransactions.retrieve(event.data.object.balance_transaction as string, {
-        stripeAccount: event.account,
-      });
-
-      if (!charge.metadata.submissionId) {
-        throw new Error('Metadata is empty');
-      }
-
-      const { error } = await supabase
-        .from('submissions')
-        .update({ price_in_grosz: balance.net })
-        .eq('id', charge.metadata.submissionId);
-
-      if (error) {
-        throw new Error(error.message);
-      }
     } catch (error) {
       return NextResponse.json(
         {
