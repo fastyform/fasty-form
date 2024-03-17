ac0b7f029a3a6a8bf7a0733ad6c621f3c15eda7b
Przemek Paziewski <ppaazz56@gmail.com>
2024-03-17 19:21:22 +0100

    feat(app): add portal wrapper, to create portals or modals, etc

--- src/components/app-modal.tsx
+++ src/components/app-modal.tsx
@@ -3,6 +3,7 @@
 import { MouseEventHandler, ReactNode, useCallback, useEffect, useRef } from 'react';
 import { Route } from 'next';
 import { useRouter } from 'next/navigation';
+import AppReactPortalWrapper from './app-react-portal-wrapper';
 
 const AppModal = ({ children, redirectUrl }: { children: ReactNode; redirectUrl: Route }) => {
   const overlay = useRef(null);
@@ -37,15 +38,17 @@ const AppModal = ({ children, redirectUrl }: { children: ReactNode; redirectUrl:
   }, [onKeyDown]);
 
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-    <div ref={overlay} className="fixed bottom-0 left-0 right-0 top-0 z-50 mx-auto bg-black/75 " onClick={onClick}>
-      <div
-        ref={wrapper}
-        className="absolute left-1/2 top-1/2 max-h-screen -translate-x-1/2 -translate-y-1/2 overflow-auto scroll-auto"
-      >
-        {children}
+    <AppReactPortalWrapper>
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+      <div ref={overlay} className="fixed bottom-0 left-0 right-0 top-0 z-50 mx-auto bg-black/75 " onClick={onClick}>
+        <div
+          ref={wrapper}
+          className="absolute left-1/2 top-1/2 max-h-screen -translate-x-1/2 -translate-y-1/2 overflow-auto scroll-auto"
+        >
+          {children}
+        </div>
       </div>
-    </div>
+    </AppReactPortalWrapper>
   );
 };
 
