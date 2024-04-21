2cacc6d2b3e00dc3bfecd8128fa7a9072deeaeee
Przemek Paziewski <ppaazz56@gmail.com>
2024-04-21 21:39:25 +0200

    feat(ambassador page): translation polish add

--- src/app/[locale]/(public)/(info)/ambassador-program/page.tsx
+++ src/app/[locale]/(public)/(info)/ambassador-program/page.tsx
@@ -1,26 +1,9 @@
-import Constants from '@/utils/constants';
-import ambassadorTosData from './utils';
+import dynamic from 'next/dynamic';
 
-const AmbassadorProgram = () => (
-  <>
-    <h1 className="text-2xl font-bold">
-      Regulamin Programu Ambasadorskiego <span className="text-yellow-400">{Constants.APP_NAME}</span>
-    </h1>
-    {Object.entries(ambassadorTosData).map(([title, descriptionPoints], titleIndex) => (
-      <div key={title} className="flex flex-col gap-5">
-        <h2 className="text-2xl">
-          {titleIndex + 1}. {title}
-        </h2>
-        {descriptionPoints.map((description, descriptionIndex) => (
-          <p
-            key={description}
-            className="ml-5"
-            dangerouslySetInnerHTML={{ __html: `${titleIndex + 1}.${descriptionIndex + 1}. ${description}` }}
-          />
-        ))}
-      </div>
-    ))}
-  </>
-);
+const AmbassadorProgram = async ({ params: { locale } }: { params: { locale: string } }) => {
+  const Component = dynamic(() => import(`./translations/${locale}.mdx`));
+
+  return <Component />;
+};
 
 export default AmbassadorProgram;
--- src/app/[locale]/(public)/(info)/layout.tsx
+++ src/app/[locale]/(public)/(info)/layout.tsx
@@ -7,7 +7,9 @@ const LegalSupportLayout = ({ children }: { children: ReactNode }) => (
   <div className="min-h-screen-responsive flex flex-col text-white">
     <PublicNavbarPlaceholder />
     <PublicContainer>
-      <PublicContainer.Content className="flex-col gap-5 py-10 md:gap-10">{children}</PublicContainer.Content>
+      <PublicContainer.Content className="flex-col gap-5 py-10 md:gap-10 [&_ol]:list-decimal [&_ol]:p-4">
+        {children}
+      </PublicContainer.Content>
     </PublicContainer>
     <PublicContainer>
       <PublicContainer.Content className="gap-5 py-5 ">
