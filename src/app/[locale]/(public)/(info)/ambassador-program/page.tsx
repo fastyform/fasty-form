01a073f5539c2a5ea9f9532b45324daf35098d29
Przemek Paziewski <ppaazz56@gmail.com>
2024-04-23 22:38:22 +0200

    feat(privacy policy): translation add

--- src/app/[locale]/(public)/(info)/ambassador-program/page.tsx
+++ src/app/[locale]/(public)/(info)/ambassador-program/page.tsx
@@ -1,9 +1,19 @@
 import dynamic from 'next/dynamic';
+import { getTranslations } from 'next-intl/server';
+import Constants from '@/utils/constants';
 
 const AmbassadorProgram = async ({ params: { locale } }: { params: { locale: string } }) => {
+  const t = await getTranslations();
   const Component = dynamic(() => import(`./translations/${locale}.mdx`));
 
-  return <Component />;
+  return (
+    <>
+      <h1 className="text-2xl font-bold">
+        {t('AMBASSADOR_TOS_PAGE_TITLE')} <span className="text-yellow-400">{Constants.APP_NAME}</span>
+      </h1>
+      <Component />
+    </>
+  );
 };
 
 export default AmbassadorProgram;
