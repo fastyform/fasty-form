8de1fb955b1952b1f8f5c268cb895a85e56b7111
Kacper Zabielski <beelzick.dev@gmail.com>
2024-05-15 23:04:03 +0200

    feat: cookies i18n

--- src/app/[locale]/(public)/(info)/cookies/cookies-declaration.tsx
+++ src/app/[locale]/(public)/(info)/cookies/cookies-declaration.tsx
@@ -1,16 +1,18 @@
 'use client';
 
 import { useQuery } from '@tanstack/react-query';
+import { useTranslations } from 'next-intl';
 import AppButton from '@/components/app-button';
+import { Locale } from '@/utils/constants';
 
 const COOKIE_DECLARATION_ID = 'cookies-declaration-id';
 
-const CookiesDeclaration = () => {
+const CookiesDeclaration = ({ locale }: { locale: Locale }) => {
+  const t = useTranslations();
   // NOTE: We need to run this code only once on client, so we use useQuery hook
   // next.js doesn't allow to use script tag without using their <Script> component so we need to add script manually
-
   useQuery({
-    queryKey: ['cookie-declaration'],
+    queryKey: ['cookie-declaration', locale],
     queryFn: () => {
       if (document.getElementById('CookieDeclaration')) return null;
 
@@ -18,6 +20,7 @@ const CookiesDeclaration = () => {
 
       const script = document.createElement('script');
       script.id = 'CookieDeclaration';
+      script.setAttribute('data-culture', locale.toUpperCase());
       script.src = 'https://consent.cookiebot.com/af9bc4d6-f7c9-4a4d-8b4f-3ace3e37cf8f/cd.js';
       content?.appendChild(script);
 
@@ -33,7 +36,7 @@ const CookiesDeclaration = () => {
           window.Cookiebot?.renew();
         }}
       >
-        Edytuj zgody
+        {t('COOKIES_EDIT_CONSENTS')}
       </AppButton>
       <div id={COOKIE_DECLARATION_ID} />
     </div>
