8e649a1e6910edb514bc9176ec219fe59bb98f55
Kacper Zabielski <86677657+beelzick@users.noreply.github.com>
2024-06-10 20:28:23 +0200

    fix: layout adjustments (#270)

--- src/app/[locale]/(app)/_components/desktop-navbar-app.tsx
+++ src/app/[locale]/(app)/_components/desktop-navbar-app.tsx
@@ -2,7 +2,7 @@ import AppLogo from '@/components/app-logo';
 import NavbarContent from './navbar-content';
 
 const DesktopNavbarApp = async () => (
-  <header className="hidden h-[68px] w-full items-center justify-center border-b border-gray-600 bg-shark lg:flex">
+  <header className="hidden h-[60px] w-full shrink-0 items-center justify-center border-b border-gray-600 bg-shark lg:flex">
     <div className="flex w-full max-w-screen-2xl items-center justify-between lg:px-5">
       <AppLogo className="w-[100px]" />
       <div className="flex h-full items-center px-5">
